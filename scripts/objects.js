ec2c6b9625167f7cba101cd3a9e822d77c056ccb
Yves Gildemeister <der.schnief@gmail.com>
2025-11-18 00:42:37 +0100

    basics: added scripts-folder

---
+++ scripts/objects.js
@@ -0,0 +1,12 @@
+const DISHES = [
+  { name: "Pizza Krabben", desc: "mit Krabben und Peperoni", price: 8.5 },
+  { name: "Pizza Margherita", desc: "mit Mozzarella", price: 5.9 },
+  {
+    name: "Pizza Diavolo (scharf)",
+    desc: "mit Salami, Zwiebeln, Peperoni und Knoblauch",
+    price: 8.5,
+  },
+  { name: "Pizzabrötchen", desc: "mit Käse und Knoblauch", price: 12.0 },
+];
+
+let cart = [];
---
+++ scripts/templates.js
@@ -0,0 +1,64 @@
+function renderMenu() {
+  let menuList = document.getElementById("menu-list");
+  menuList.innerHTML = `
+    <img src="">
+    <h3>Hauptgerichte</h3>`;
+
+  for (let index = 0; index < DISHES.length; index++) {
+    let dish = DISHES[index];
+
+    if (dish.price > 0) {
+      menuList.innerHTML += `
+        <div class="menu_item">
+          <div class="menu_item_text">
+            <h4>${dish.name}</h4>
+            <p>${dish.desc}</p>
+            <span>${dish.price.toFixed(2)}€</span>
+          </div>
+          <div class="menu_item_button" onclick="addToCart(${index})">+</div>
+          
+        </div>
+      `;
+    }
+  }
+}
+
+function renderCart() {
+  let cartList = document.getElementById("cartList");
+  let subtotalRef = document.getElementById("subtotal");
+  let totalRef = document.getElementById("total");
+  let shipping = 5.0;
+
+  cartList.innerHTML = "";
+
+  let subtotal = 0;
+
+  cart.forEach((item, index) => {
+    let totalItem = item.price * item.quantity;
+    subtotal += totalItem;
+
+    cartList.innerHTML += `
+        <div class="cart-item">
+
+            <div>
+              <h4>${item.name}</h4>
+              <p>
+                <button onclick="changeQuantity(${index}, -1)">-</button>
+                ${item.quantity}
+                <button onclick="changeQuantity(${index}, 1)">+</button>
+                x ${item.price.toFixed(2)}€
+              </p>
+            </div>
+
+            <div>
+              <span>${totalItem.toFixed(2)}€</span>
+              <button onclick="changeQuantity(${index}, -item.quantity)">🗑️</button>
+            </div>
+
+        </div>
+    `;
+  });
+
+  subtotalRef.textContent = `${subtotal.toFixed(2)}€`;
+  totalRef.textContent = `${(subtotal + shipping).toFixed(2)}€`;
+}
